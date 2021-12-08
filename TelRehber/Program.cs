6801a82232ef11976b5bf8bf432a28efdffe74a1
sarperkaya01 <sarperkaya01@gmail.com>
2021-12-08 13:40:42 +0300

    Add project files.

---
+++ TelRehber/Program.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelRehber
+{
+    class Program : Rehber
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Kisiler kisi = new Kisiler();
+
+
+
+
+            Console.WriteLine("İşlem menusune hoşgeldiniz...");
+            Console.WriteLine("*************************");
+
+
+            Console.WriteLine("1) Yeni Numara Kaydet");
+            Console.WriteLine("2) Varolan Numarayı Silme");
+            Console.WriteLine("3) Numara Güncelleme");
+            Console.WriteLine("4) Rehberi Listele");
+            Console.WriteLine("5) Rehberde arama yap");
+
+
+            bool k = true;
+
+            while (k)
+            {
+                Console.Write("Yapmak istediğiniz işlemi girini :");
+                int islem = int.Parse(Console.ReadLine());
+                switch (islem)
+                {
+                    case 1:
+                        Console.Write("Lütfen isim giriniz             : ");
+                        string x = Console.ReadLine();
+
+                        Console.Write("Lütfen soyisim giriniz          :");
+                        string y = Console.ReadLine();
+
+                        Console.Write("Lütfen telefon numarası giriniz :");
+                        int z = int.Parse(Console.ReadLine());
+                        kisi.Ekle(x, y, z);
+
+
+                        Console.WriteLine("*************************");
+                        break;
+                    case 2:
+
+                        bool u = true;
+                        while (u)
+                        {
+                            Console.WriteLine("Silmek istediğiniz kişinin adını girin");
+                            string n = Console.ReadLine();
+                            kisi.AramaName(n);
+                            kisi.AramaListele();
+                            bool kont = kisi.varMi(n);
+                            if (kont == false)
+                            {
+                                bool w = true;
+                                while (w)
+                                {
+
+
+                                    Console.WriteLine("Aradığınız krtiterlere uygun veri rehberde bulunamadı. Lütfen bir seçim yapınız.");
+                                    Console.WriteLine("Silmeyi sonlandırmak için : (1)");
+                                    Console.WriteLine("Yeniden denemek için      : (2)");
+                                    string r = Console.ReadLine();
+
+                                    switch (r)
+                                    {
+                                        case "1":
+                                            w = false;
+                                            u = false;
+                                            Console.WriteLine("Anamenüye aktarılıyorsunuz...");
+                                            break;
+                                        case "2":
+                                            w = false;
+                                            u = true;
+                                            break;
+                                        default:
+                                            Console.WriteLine("Hatalı giriş yaptınız yeniden deneyin. Anamenüye aktarılıyorsunuz...");
+                                            w = false;
+                                            break;
+
+                                    }
+                                }
+                            }
+                            else
+                            {
+
+
+                                bool s = true;
+                                while (s)
+                                {
+                                    Console.WriteLine("Silme işlemini onaylıyor musunuz (y/n)");
+                                    string p = Console.ReadLine();
+
+                                    if (p == "y")
+                                    {
+                                        kisi.Sil(n);
+                                        Console.WriteLine("Silme işlemi başarılı");
+                                        s = false;
+                                        u = false;
+                                    }
+                                    else if (p == "n")
+                                    {
+                                        Console.WriteLine("Silme işlemi iptal edildi");
+                                        s = false;
+                                        u = false;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Hatalı giriş yaptınız yeniden deneyin.");
+
+
+                                    }
+                                }
+                            }
+
+
+                        }
+
+
+                        break;
+
+                    case 3:
+                        
+
+
+                        bool q = true;
+                        while (q)
+                        {
+                            Console.WriteLine("Güncellemek istediğiniz kişinin adını girin");
+                            string n = Console.ReadLine();
+                            bool kont = kisi.varMi(n);
+                            if (kont == false)
+                            {
+                                bool w = true;
+                                while (w)
+                                {
+
+
+                                    Console.WriteLine("Aradığınız krtiterlere uygun veri rehberde bulunamadı. Lütfen bir seçim yapınız.");
+                                    Console.WriteLine("Güncellemeyi sonlandırmak için : (1)");
+                                    Console.WriteLine("Yeniden denemek için      : (2)");
+                                    string r = Console.ReadLine();
+
+                                    switch (r)
+                                    {
+                                        case "1":
+                                            w = false;
+                                            q = false;
+                                            Console.WriteLine("Anamenüye aktarılıyorsunuz...");
+                                            break;
+                                        case "2":
+                                            w = false;
+                                            q = true;
+                                            break;
+                                        default:
+                                            Console.WriteLine("Hatalı giriş yaptınız yeniden deneyin. Anamenüye aktarılıyorsunuz...");
+                                            w = false;
+                                            break;
+
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                //Console.WriteLine("İsim/Soyisim güncellemesi için 1'"); bu sonra yapılacak bu bloğun içinden yapılabilir
+
+                                Console.WriteLine("Güncellemek isediğiniz numarayı yazın");
+                                int yeni = int.Parse(Console.ReadLine());
+                                bool s = true;
+                                while (s)
+                                {
+                                    Console.WriteLine("Güncellemek isediğiniz numaraya " + yeni);
+                                    Console.WriteLine("Güncelleme işlemini onaylıyor musunuz (y/n)");
+                                    string p = Console.ReadLine();
+
+                                    if (p == "y")
+                                    {
+
+
+                                        kisi.GuncelleNum(n, yeni);
+                                        Console.WriteLine("Güncelleme işlemi başarılı");
+                                        s = false;
+                                        q = false;
+                                    }
+                                    else if (p == "n")
+                                    {
+                                        Console.WriteLine("Güncelleme işlemi iptal edildi");
+                                        s = false;
+                                        q = false;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Hatalı giriş yaptınız yeniden deneyin.");
+
+
+                                    }
+                                }
+
+                            }
+
+                        }
+                        break;
+                    case 4:
+                        kisi.Listele();
+
+                        Console.WriteLine("*************************");
+                        break;
+                    case 5:
+                        bool m = true;
+                        while (m)
+                        {
+                            Console.WriteLine("Arama yapmak istediğiniz tipi seçiniz.");
+                            Console.WriteLine("**********************************************");
+                            Console.WriteLine("İsim veya soyisime göre arama yapmak için: (1)");
+                            Console.WriteLine("Telefon numarasına göre arama yapmak için: (2)");
+                            string h = Console.ReadLine();
+                            if (h == "1")
+                            {
+
+
+
+                                Console.WriteLine("Rehberde aramak istediğiniz ismi giriniz");
+                                string n = Console.ReadLine();
+                                bool kont = kisi.varMi(n);
+                                if (kont == false)
+                                {
+                                    bool w = true;
+                                    while (w)
+                                    {
+
+
+                                        Console.WriteLine("Aradığınız krtiterlere uygun veri rehberde bulunamadı. Lütfen bir seçim yapınız.");
+                                        Console.WriteLine("Güncellemeyi sonlandırmak için : (1)");
+                                        Console.WriteLine("Yeniden denemek için      : (2)");
+                                        string r = Console.ReadLine();
+
+                                        switch (r)
+                                        {
+                                            case "1":
+                                                w = false;
+                                                m = false;
+                                                Console.WriteLine("Anamenüye aktarılıyorsunuz...");
+                                                break;
+                                            case "2":
+                                                w = false;
+                                                m = true;
+                                                break;
+                                            default:
+                                                Console.WriteLine("Hatalı giriş yaptınız yeniden deneyin. Anamenüye aktarılıyorsunuz...");
+                                                m = false;
+                                                break;
+
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("*******************");
+                                    kisi.AramaName(n);
+                                    kisi.AramaListele();
+
+                                    m = false;
+                                }
+
+
+                            }
+                            else if (h == "2")
+                            {
+                                Console.WriteLine("Rehberde aramak istediğiniz numarayı giriniz");
+                                int n = int.Parse(Console.ReadLine());
+                                bool kont = kisi.varMiNum(n);
+                                if (kont == false)
+                                {
+                                    bool w = true;
+                                    while (w)
+                                    {
+
+
+                                        Console.WriteLine("Aradığınız krtiterlere uygun veri rehberde bulunamadı. Lütfen bir seçim yapınız.");
+                                        Console.WriteLine("Güncellemeyi sonlandırmak için : (1)");
+                                        Console.WriteLine("Yeniden denemek için      : (2)");
+                                        string r = Console.ReadLine();
+
+                                        switch (r)
+                                        {
+                                            case "1":
+                                                w = false;
+                                                m = false;
+                                                Console.WriteLine("Anamenüye aktarılıyorsunuz...");
+                                                break;
+                                            case "2":
+                                                w = false;
+                                                m = true;
+                                                break;
+                                            default:
+                                                Console.WriteLine("Hatalı giriş yaptınız yeniden deneyin. Anamenüye aktarılıyorsunuz...");
+                                                m = false;
+                                                break;
+
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    int f = n;
+                                    kisi.AramaNum(f);
+                                    kisi.AramaListele();
+
+                                    m = false;
+
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Hatalı giriş yaptınız yeniden deneyin.");
+
+                            }
+
+                        }
+
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Hatalı giriş yaptınız tekrar deneyin...");
+                        break;
+                }
+            }
+
+
+            Console.ReadLine();
+
+
+
+        }
+
+
+    }
+
+}
---
+++ TelRehber/Rehber.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelRehber
+{
+    class Rehber
+    {
+
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public int num { get; set; }
+
+        ArrayList isimler = new ArrayList();
+        ArrayList soyisimler = new ArrayList();
+        ArrayList nums = new ArrayList();
+        ArrayList altisim = new ArrayList();
+        ArrayList altSoy = new ArrayList();
+        ArrayList altnum = new ArrayList();
+        public Rehber()
+        {
+
+            isimler.Add("Necla");
+            soyisimler.Add("Kaya");
+            nums.Add(11111);
+
+            isimler.Add("Kaya");
+            soyisimler.Add("KaraKaya");
+            nums.Add(22222);
+
+            isimler.Add("Sarp");
+            soyisimler.Add("Para");
+            nums.Add(33333);
+
+            isimler.Add("Sarp");
+            soyisimler.Add("Kaymaz");
+            nums.Add(44444);
+
+
+            isimler.Add("Alp");
+            nums.Add(55555);
+            soyisimler.Add("Kayar");
+
+
+
+        }
+
+
+
+        public void Listele()
+        {
+            for (int i = 0; i < nums.Count; i++)
+            {
+                Console.WriteLine("İsim:" + isimler[i]);
+                Console.WriteLine("Soyisim:" + soyisimler[i]);
+                Console.WriteLine(nums[i]);
+                Console.WriteLine("--");
+            }
+
+        }
+        public void Ekle(string name, string sur, int numb)
+        {
+            isim = name;
+            isimler.Add(isim);
+
+            soyisim = sur;
+            soyisimler.Add(soyisim);
+
+            num = numb;
+            nums.Add(num);
+        }
+        public void Sil(string name)
+        {
+
+            int b = isimler.IndexOf(name);
+            int c = soyisimler.IndexOf(name);
+            if (b < 0)
+            {
+                isimler.RemoveAt(c);
+
+
+
+                soyisimler.RemoveAt(c);
+
+
+                nums.RemoveAt(c);
+            }
+            else
+            {
+                isimler.RemoveAt(b);
+
+
+
+                soyisimler.RemoveAt(b);
+
+
+                nums.RemoveAt(b);
+            }
+
+
+        }
+        public void GuncelleNum(string name, int newnum)
+        {
+            int b = isimler.IndexOf(name);
+            int c = soyisimler.IndexOf(name);
+            if (b < 0)
+            {
+
+
+                nums.RemoveAt(c);
+                nums.Insert(c, newnum);
+            }
+            else
+            {
+
+                nums.RemoveAt(b);
+                nums.Insert(b, newnum);
+            }
+        }
+        public void GuncelleName(string name, string newname) 
+        {
+            int b = isimler.IndexOf(name);
+            int c = soyisimler.IndexOf(name);
+            if (b < 0)
+            {
+                soyisimler.RemoveAt(c);
+                soyisimler.Insert(c, newname);
+                
+            }
+            else
+            {
+
+                isimler.RemoveAt(b);
+                isimler.Insert(b, newname);
+            }
+        }
+
+        public bool varMi(string name)
+        {
+            bool x = false;
+            bool kontrol = true;
+            int sayac = 0;
+            while (kontrol)
+            {
+                foreach (var item in isimler)
+                {
+                    x = item.Equals(name);
+                    if (x == true)
+                    {
+                        kontrol = false;
+                        break;
+                    }
+
+                }
+                foreach (var item in soyisimler)
+                {
+                    if (kontrol == false)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        x = item.Equals(name);
+
+                        if (x == true)
+                        {
+                            kontrol = false;
+                            break;
+                        }
+                        else
+                        {
+                            sayac++;
+                        }
+                    }
+
+                }
+                if (sayac == soyisimler.Count)
+                {
+
+
+                    kontrol = false;
+                    x = false;
+                }
+                else
+                {
+                    kontrol = false;
+                }
+
+
+
+            }
+
+
+            return x;
+
+        }
+        public bool varMiNum(int num) 
+        {
+            bool a = false;
+            bool kontrol = true;
+            int sayac = 0;
+            while (kontrol)
+            {
+                foreach (var item in nums)
+                {
+                    if (kontrol == false)
+                    {
+                        break;
+                    }
+                    else
+                    {
+
+
+                        a = item.Equals(num);
+
+                        if (a == true)
+                        {
+                            kontrol = false;
+                            break;
+                        }
+                        else
+                        {
+                            sayac++;
+                        }
+                    }
+
+                }
+                if (sayac == soyisimler.Count)
+                {
+
+
+                    kontrol = false;
+                    a = false;
+                }
+                else
+                {
+                    kontrol = false;
+                }
+            }
+            return a;
+
+        }
+
+        
+        public void AramaName(string name) 
+        {
+            if (altisim.Count<=0 && altSoy.Count<=0)
+            {
+                for (int i = 0; i < isimler.Count; i++)
+                {
+
+                    if (Convert.ToString(isimler[i]) == name)
+                    {
+                        altisim.Add(isimler[i]);
+                        altSoy.Add(soyisimler[i]);
+                        altnum.Add(nums[i]);
+                    }
+
+
+                }
+                for (int i = 0; i < soyisimler.Count; i++)
+                {
+
+                    if (Convert.ToString(soyisimler[i]) == name)
+                    {
+                        altisim.Add(isimler[i]);
+                        altSoy.Add(soyisimler[i]);
+                        altnum.Add(nums[i]);
+                    }
+
+
+                }
+            }
+            else
+            {
+                AltSil();
+                for (int i = 0; i < isimler.Count; i++)
+                {
+
+                    if (Convert.ToString(isimler[i]) == name)
+                    {
+                        altisim.Add(isimler[i]);
+                        altSoy.Add(soyisimler[i]);
+                        altnum.Add(nums[i]);
+                    }
+
+
+                }
+                for (int i = 0; i < soyisimler.Count; i++)
+                {
+
+                    if (Convert.ToString(soyisimler[i]) == name)
+                    {
+                        altisim.Add(isimler[i]);
+                        altSoy.Add(soyisimler[i]);
+                        altnum.Add(nums[i]);
+                    }
+
+
+                }
+            }
+            
+
+        }
+        public void AramaNum(int num) 
+        {
+            if (altnum.Count<=0)
+            {
+                int b = nums.IndexOf(num);
+                if (b < 0)
+                {
+                    Console.WriteLine("Bu numara rehberde bulunmamaktadır");
+
+                }
+                else
+                {
+                    altisim.Add(isimler[b]);
+
+
+
+                    altSoy.Add(soyisimler[b]);
+
+
+                    altnum.Add(nums[b]);
+                }
+            }
+            else
+            {
+                AltSil();
+                int b = nums.IndexOf(num);
+                if (b < 0)
+                {
+                    Console.WriteLine("Bu numara rehberde bulunmamaktadır");
+
+                }
+                else
+                {
+                    altisim.Add(isimler[b]);
+
+
+
+                    altSoy.Add(soyisimler[b]);
+
+
+                    altnum.Add(nums[b]);
+                }
+            }
+            
+            
+        }
+        public void AramaListele()
+        {
+
+            for (int i = 0; i < altnum.Count; i++)
+            {
+                Console.WriteLine("İsim:" + altisim[i]);
+                Console.WriteLine("Soyisim:" + altSoy[i]);
+                Console.WriteLine(altnum[i]);
+                Console.WriteLine("--");
+            }
+
+        }
+        public void AltSil() 
+        {
+            
+                altisim.Clear();
+
+
+
+                altSoy.Clear();
+
+
+                altnum.Clear();
+           
+        }
+
+
+    }
+}
